59754dd7a9097ca985a4be7c859c87fb8dde9bc8
Celio Caus Junior <ccaus@uol.com.br>
2019-08-15 18:46:20 -0300

    ItemPedido e ItemPedidoPk

--- src/main/java/com/celiocaus/cursomc/CursomcApplication.java
+++ src/main/java/com/celiocaus/cursomc/CursomcApplication.java
@@ -13,6 +13,7 @@ import com.celiocaus.cursomc.domain.Cidade;
 import com.celiocaus.cursomc.domain.Cliente;
 import com.celiocaus.cursomc.domain.Endereco;
 import com.celiocaus.cursomc.domain.Estado;
+import com.celiocaus.cursomc.domain.ItemPedido;
 import com.celiocaus.cursomc.domain.Pagamento;
 import com.celiocaus.cursomc.domain.PagamentoComBoleto;
 import com.celiocaus.cursomc.domain.PagamentoComCartao;
@@ -25,6 +26,7 @@ import com.celiocaus.cursomc.repositories.CidadeRepository;
 import com.celiocaus.cursomc.repositories.ClienteRepository;
 import com.celiocaus.cursomc.repositories.EnderecoRepository;
 import com.celiocaus.cursomc.repositories.EstadoRepository;
+import com.celiocaus.cursomc.repositories.ItemPedidoRepository;
 import com.celiocaus.cursomc.repositories.PagamentoRepository;
 import com.celiocaus.cursomc.repositories.PedidoRepository;
 import com.celiocaus.cursomc.repositories.ProdutoRepository;
@@ -48,6 +50,8 @@ public class CursomcApplication implements CommandLineRunner {
 	private PedidoRepository pedidoRepository;
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 	
 	
 
@@ -118,6 +122,18 @@ public class CursomcApplication implements CommandLineRunner {
 		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
 		
 
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+		
+		ped1.getItens().addAll(Arrays.asList(ip1,ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+		
+		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
 	
 	
 	}
--- src/main/java/com/celiocaus/cursomc/domain/ItemPedido.java
+++ src/main/java/com/celiocaus/cursomc/domain/ItemPedido.java
@@ -2,10 +2,15 @@ package com.celiocaus.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+@Entity
+
 public class ItemPedido implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	@EmbeddedId
 	private ItemPedidoPk id = new ItemPedidoPk();
 
 	private Double desconto;
